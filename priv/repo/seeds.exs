64773311c3a298e52cf194e03d9bd46771b361c1
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-08 19:36:16 -0400

    added faker library

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,15 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Server.Repo.insert!(%Server.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Server.{Posts, Repo}
+
+mock_photos = 9
+
+photos_list = [
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532452457/wide.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532452456/tall.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532454234/saga/pexels-photo-1191668.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532454234/saga/pexels-photo-996635.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532454233/saga/pexels-photo-839773.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532454233/saga/pexels-photo-577585.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532452456/saga/delicate-arch-night-stars-landscape.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1532452456/saga/industry-sunrise.jpg",
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1528082073/saga/abstract_V04.jpg"
+]
