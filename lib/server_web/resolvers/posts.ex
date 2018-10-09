dfaf8be9897bb7b552d396373bc86d462b55f50d
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-08 20:15:57 -0400

    attached id argument to photo query

--- lib/server_web/resolvers/posts.ex
+++ lib/server_web/resolvers/posts.ex
@@ -2,4 +2,8 @@ defmodule ServerWeb.Resolvers.Posts do
   def photos(_, _, _) do
     {:ok, Server.Posts.list_photos()}
   end
+
+  def photo(_, %{id: id}, _) do
+    {:ok, Server.Posts.get_photo!(id)}
+  end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,13 +11,14 @@ photos_list = [
   "https://res.cloudinary.com/bracket-factory/image/upload/v1532454233/saga/pexels-photo-577585.jpg",
   "https://res.cloudinary.com/bracket-factory/image/upload/v1532452456/saga/delicate-arch-night-stars-landscape.jpg",
   "https://res.cloudinary.com/bracket-factory/image/upload/v1532452456/saga/industry-sunrise.jpg",
-  "https://res.cloudinary.com/bracket-factory/image/upload/v1528082073/saga/abstract_V04.jpg"
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1528082073/saga/abstract_V04.jpg",
+  "https://freestocks.org/fs/wp-content/uploads/2016/08/timber-800x533.jpg"
 ]
 
 for idx <- 0..mock_photos do
   photo = %{
     image_url: Enum.at(photos_list, idx),
-    caption: Faker.Lorem.Shakespeare.king_richard_iii()
+    caption: Faker.Lorem.Shakespeare.hamlet()
   }
 
   %Posts.Photo{}
