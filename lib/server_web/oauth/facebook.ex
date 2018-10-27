642633c83a23224a4107b2d4d8473a22b5a9720b
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-10-27 18:13:52 -0400

    created function to normalize facebook authorization data

--- lib/server_web/oauth/facebook.ex
+++ lib/server_web/oauth/facebook.ex
@@ -3,11 +3,29 @@ defmodule ServerWeb.Oauth.Facebook do
 
   def get_info(token) do
     token
+    # user
     |> get_user
+    |> get_user_profile_picture("small", token)
+    |> normalize
   end
 
   defp get_user(token) do
     {:ok, user} = Facebook.me([fields: @fields], token)
     user
+  end
+
+  defp get_user_profile_picture(user, size, token) do
+    {:ok, data} = Facebook.picture(user["id"], size, token)
+    [user, data]
+  end
+
+  defp normalize([user, picture_data]) do
+    %{
+      facebook_id: user["id"],
+      avatar: picture_data["data"]["url"],
+      first_name: user["first_name"],
+      last_name: user["last_name"],
+      email: user["email"]
+    }
   end
 end
